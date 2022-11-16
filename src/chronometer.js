5adbcbd891e386f35ed500689d06fc56fa3ad78e
MoniCamargo37 <moni28ac@gmail.com>
2022-11-16 21:54:22 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime += 1;
+    }, 1000);
   }
-
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return String("0" + value).slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.currentTime);
+    console.log(`stop ${this.currentTime}`);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
-
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return minutes + ":" + seconds;
   }
 }
